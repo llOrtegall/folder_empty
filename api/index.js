d048e57124ef6ff882c6c8a6e6113f18ca4dd0d8
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-31 10:11:12 -0500

    agregando el token

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotennv = require('dotenv')
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
 
 dotennv.config();
 mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 
@@ -11,9 +14,14 @@ app.get('/test/', (req, res) => {
   res.json('test ok');
 })
 
-app.post('/register', (req, res) => {
-
-})
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const createUser = await User.create({ username, password });
+  jwt.sign({ userId: createUser._id }, jwtSecret, (err, token) => {
+    if (err) throw err;
+    res.cookie('token', token).status(201).json('ok');
+  });
+});
 
 app.listen(4000);
 
