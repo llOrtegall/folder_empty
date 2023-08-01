2d995c0835f72676bf17d6343d93da8d07f0db35
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-01 15:27:42 -0500

    DESDE 0 POR TERCERA VEZ

--- api/models/User.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose'
-
-const UserSchema = new Schema({
-  username: { type: String, unique: true },
-  password: String
-}, { timestamps: true })
-
-export const UserModel = model('User', UserSchema)
