3ef84446d2f76d0356ae86a848216d49003c23da
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-01 10:32:35 -0500

    agregando axios app client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import axios from "axios";
 import Register from "./Register";
 
 function App() {
-
+  axios.defaults.baseURL = "http://localhost:4000";
+  axios.defaults.withCredentials = true;
   return (
     <Register />
   )
