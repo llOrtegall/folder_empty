db880e39ceda89f7250719ee1b21af1c66cb8510
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-13 12:07:16 -0500

    agregando states user and pass

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
+
 export function Register() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="bg-blue-100 h-screen flex items-center">
       <form className="w-64 mx-auto ">
-        <input type="text" placeholder="Usuario" className="block w-full rounded-sm p-2 mb-2" />
-        <input type="password" placeholder="Contraseña" className="block w-full rounded-sm p-2 mb-2" />
+        <input value={username}
+          onChange={ev => setUsername(ev.target.value)}
+          type="text" placeholder="Usuario"
+          className="block w-full rounded-sm p-2 mb-2" />
+        <input value={password}
+          onChange={ev => setPassword(ev.target.value)}
+          type="password" placeholder="Contraseña"
+          className="block w-full rounded-sm p-2 mb-2" />
         <button className="bg-blue-500 text-white block w-full rounded-sm p-2">Register</button>
       </form>
     </div>
