2bfe80b95c15dd6d1dbe64786f6b1eb6ee160a5d
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-13 12:14:30 -0500

    creando la api

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -7,7 +7,7 @@ export function Register() {
 
   return (
     <div className="bg-blue-100 h-screen flex items-center">
-      <form className="w-64 mx-auto ">
+      <form className="w-64 mx-auto mb-12">
         <input value={username}
           onChange={ev => setUsername(ev.target.value)}
           type="text" placeholder="Usuario"
