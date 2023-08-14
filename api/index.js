15ace4811c1bbc0510ece0a7460102d59c813055
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-14 12:21:13 -0500

    conectando con base de datos

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+// ** CONECTA CON BASE DE DATOS
+mongoose.connect(process.env.MONGO_URL)
 
 const PORT = 4040
 const app = express();
@@ -7,6 +13,9 @@ app.get('/test', (req, res) => {
   res.json('test ok ¡¡¡')
 })
 
+app.post('/register', (req, res) => {
+
+})
 
 console.log('Inicializado en el servidor: ' + PORT);
 app.listen(PORT);
