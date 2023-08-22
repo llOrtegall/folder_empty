6621a0eb9d5698763a18658e5f00694dfa15611f
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-22 17:32:52 -0500

    visual register ok

--- cliente/src/components/Register.jsx
+++ cliente/src/components/Register.jsx
@@ -1,7 +1,15 @@
 export function Register() {
   return (
-    <div>
-
-    </div>
+    <section className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto">
+        <input type="text" placeholder="Usuario"
+          className="block w-full rounded-sm p-2 mb-2 border" />
+        <input type="password" placeholder="Contraseña"
+          className="block w-full rounded-sm p-2 mb-2 border" />
+        <button className="bg-blue-500 text-white block w-full p-2 rounded-md">
+          Registrase
+        </button>
+      </form>
+    </section>
   )
-}
+} 
