86fd0956bcbddd855c49997edc8b19809261b2fa
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-24 21:16:14 -0500

    login ok

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import { connect } from 'mongoose'
 import { config } from 'dotenv'
 import { UserModel } from './models/User.js'
 import cors from 'cors'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser'
 
 config()
 connect(process.env.MONGO_URL)
 const jwtSecret = process.env.JWT_SECRET
+const bcryptSalt = bcrypt.genSaltSync(10)
 
 const app = express()
 app.use(express.json())
@@ -34,11 +36,31 @@ app.get('/profile', (req, res) => {
   }
 })
 
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  const foundUser = await UserModel.findOne({ username })
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password)
+    if (passOk) {
+      jwt.sign({ UserId: foundUser._id, username }, jwtSecret, {},
+        (_err, token) => {
+          res.cookie('token', token, { sameSite: 'none', secure: true }).json({
+            id: foundUser._id
+          })
+        })
+    }
+  }
+})
+
 // TODO: para realizar el registro de los usuarios
 app.post('/register', async (req, res) => {
   const { username, password } = req.body
   try {
-    const createdUser = await UserModel.create({ username, password })
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
+    const createdUser = await UserModel.create({
+      username,
+      password: hashedPassword
+    })
     // TODO: creamos el token
     jwt.sign({ UserId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
       if (err) throw err
