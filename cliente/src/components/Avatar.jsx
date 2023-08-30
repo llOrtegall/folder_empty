c6c3669a0cd7baba9494dadceba614bc6fcfafc7
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-30 09:47:45 -0500

    organizado que este offline

--- cliente/src/components/Avatar.jsx
+++ cliente/src/components/Avatar.jsx
@@ -16,8 +16,11 @@ export function Avatar({ userId, username, online }) {
         {username[0]}
       </div>
       {online && (
-        <div className="absolute w-3 h-3 bg-green-600 bottom-0 right-0 rounded-full border-0 border-white"></div>
+        <div className="absolute w-4 h-3 bg-green-600 bottom-0 right-0 rounded-full border-2 border-white"></div>
+      )}
+      {!online && (
+        <div className="absolute w-4 h-3 bg-gray-400 bottom-0 right-0 rounded-full border-2 border-white"></div>
       )}
     </section >
-  )
+  );
 }
