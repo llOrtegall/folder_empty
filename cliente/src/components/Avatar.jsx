edbc8aaa3fc73d8f2bf431fff342325316e24eb6
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-30 08:03:51 -0500

    agregando botton online

--- cliente/src/components/Avatar.jsx
+++ cliente/src/components/Avatar.jsx
@@ -1,17 +1,23 @@
 // eslint-disable-next-line react/prop-types
-export function Avatar({ userId, username }) {
+export function Avatar({ userId, username, online }) {
 
-  const colors = ['bg-red-200', 'bg-green-200', 'bg-orange-200', 'bg-yellow-200', 'bg-cyan-200', 'bg-purple-200']
+  const colors = [
+    'bg-purple-200', 'bg-red-200',
+    'bg-orange-200', 'bg-green-200',
+    'bg-red-200', 'bg-cyan-200']
 
   const userIdBase10 = parseInt(userId, 24);
   const colorIndex = userIdBase10 % colors.length;
   const color = colors[colorIndex];
 
   return (
-    <section className={`w-8 h-8 ${color} rounded-full flex items-center`}>
+    <section className={`${color} w-8 h-8 relative rounded-full flex items-center`}>
       <div className="text-center w-full opacity-80 uppercase font-semibold">
         {username[0]}
       </div>
+      {online && (
+        <div className="absolute w-3 h-3 bg-green-600 bottom-0 right-0 rounded-full border-0 border-white"></div>
+      )}
     </section >
   )
 }
--- cliente/src/components/Chat.jsx
+++ cliente/src/components/Chat.jsx
@@ -95,7 +95,7 @@ export function Chat() {
               <div className='w-1 bg-blue-500 h-12 rounded-r-md'></div>
             )}
             <div className='flex gap-2 py-2 pl-4 items-center'>
-              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <Avatar online={true} username={onlinePeople[userId]} userId={userId} />
               <span className='text-gray-800 font-semibold'>
                 {onlinePeople[userId]}
               </span>
