ea27b3fbb0253e87ef5856802143be734421f968
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-13 22:27:09 -0500

    EJEMPLOS DE USO QUERYS

--- server/db.ts
+++ server/db.ts
@@ -21,12 +21,29 @@ interface IUserRow extends RowDataPacket {
 //   return results as T[];
 // }
 
+// SELECT * FROM usuarios
 const SelectQuery = async <T>(queryString: string): Promise<T[]> => {
   const [results] = await pool.execute(queryString) // TODO: desctructure the results
   return results as T[];
 }
 
-SelectQuery<IUserRow>('SELECT * FROM usuarios')
-  .then((users) => { console.log(users) })
-  .catch((err) => { console.error(err) })
+//  TODO: INSERT, UPDATE, DELETE
+const ModifyQuery = async (queryString: string): Promise<ResultSetHeader> => {
+  const [results] = await pool.execute(queryString)
+  return results as ResultSetHeader
+}
+
+// ModifyQuery('INSERT INTO usuarios (nombre, correo) VALUES ("Ivan Ortega", "IvanOrtega@example.com")')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.error(err))
+
+
+// SelectQuery<IUserRow>('SELECT * FROM usuarios')
+//   .then((users) => { 
+//     users.forEach((user) => {
+//       const { id, nombre, correo } = user
+//       console.log(`id: ${id}, nombre: ${nombre}, correo: ${correo}`)
+//     })
+//    })
+//   .catch((err) => { console.error(err) })
 
